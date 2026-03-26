e31e55016450839863f117890f2afda2f4249ab4
romely126 <vega0101938@gmail.com>
2026-03-26 23:52:55 +0900

    26-03-26

---
+++ section08/src/components/Editor.jsx
@@ -0,0 +1,14 @@
+import "./Editor.css"
+
+const Editor = () => {
+
+
+    return (
+        <div className="Editor">
+            <input placeholder="새로운 ToDo" />
+            <button>추가</button>
+        </div>
+    )
+}
+
+export default Editor;
