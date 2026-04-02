4b209ec7fcd5a5463854cccf58f9d07e614a4d12
romely126 <vega0101938@gmail.com>
2026-04-02 11:24:38 +0900

    26-04-02

--- section08/src/App.jsx
+++ section08/src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css"
-import { useState } from "react";
+import { useState, useRef } from "react";
 import Header from "./components/Header"
 import Editor from "./components/Editor"
 import List from "./components/List"
@@ -18,7 +18,7 @@ const mockData = [
       date : new Date().getTime(),
     },
     {
-      id: 0,
+      id: 2,
       isDone: false,
       content : "노래 연습하기",
       date : new Date().getTime(),
@@ -27,13 +27,39 @@ const mockData = [
 
 function App() {
   
-  const [todos, setTodos] = useState({mockData});
+  const [todos, setTodos] = useState(mockData);
+  const idRef = useRef(3);
+
+  const onCreate = (content) => {
+    const newTodo = {
+      id: idRef.current++,
+      isDone: false,
+      content: content,
+      date: new Date().getTime()
+    };
+
+    setTodos([newTodo, ...todos]);
+  };
+
+  const onUpdate = (targetId) => {
+    setTodos(
+      todos.map((todo)=>
+        todo.id === targetId
+        ? {...todo, isDone:!todo.isDone} 
+        : todo
+      )
+    );
+  }
+
+  const onDelete = (targetId) => {
+    setTodos(todos.filter((todo)=> todo.id !== targetId));
+  }
 
   return (
       <div className="App">
         <Header />
-        <Editor />
-        <List />
+        <Editor onCreate={onCreate}/>
+        <List todos={todos} onUpdate={onUpdate} onDelete={onDelete}/>
       </div>
   )
 }
